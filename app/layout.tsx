1a397f45f751f98d3e72b6d1d9169f161050436d
meanev56 <emekanjoku86@gmail.com>
2024-07-19 15:11:33 +0100

    forms

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = FontSans({ 
   subsets: ["latin"], 
@@ -23,7 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased", fontSans.variable)}>{children}</body>
+      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased", fontSans.variable)}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+          >
+            {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
