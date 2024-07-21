314473a9d6eb84a2873eef4efe23119cf2f46447
meanev56 <emekanjoku86@gmail.com>
2024-07-21 10:43:51 +0100

    appwrite

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import PatientForm from "@/components/forms/PatientForm";
+
+import { PatientForm } from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
