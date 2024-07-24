c49b97ab8dfa32e1252723e4c3f0029cd749831b
meanev56 <emekanjoku86@gmail.com>
2024-07-24 03:04:30 +0100

    assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,44 @@
+import PatientForm from '@/components/forms/PatientForm/PatientForm'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
-import { PasskeyModal } from "@/components/PasskeyModal";
-import { PatientForm } from "@/components/forms/PatientForm";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-
-export default function Home({ searchParams }: SearchParamProps) {
-  const isAdmin = searchParams?.admin === "true";
- 
+const page = () => {
   return (
-    <div className="flex h-screen max-h-screen" >
-      {isAdmin && <PasskeyModal />}
-
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
-          <Image
+    <div className='flex h-screen max-h-screen'>
+       <section className='remove-scrollbar container my-auto'>
+        <div className='sub-container max-w-[496px]'>
+          <Image 
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
+            alt='patient'
+            className='mb-12 h-10 w-fit'
           />
+
           <PatientForm />
-          <div className="text-14-regular mt-20 flex justify-between ">
-             <p className="justify-itens-end text-dark-600 xl:text-left">
-             © 2024 CarePluse
-              </p>
-              <Link href="/?admin=true" className="text-green-500">
-                Admin
-              </Link>
+
+          <div className='text-14-regular mt-20 flex justify-between'>
+            <p className='justify-items-end text-dark-600 xl:text-left'>
+              © 2024 CarePluse
+            </p>
+            <Link href='/?admin=true' className='text-green-500'>
+              Admin
+            </Link>
           </div>
         </div>
-      </section>
+       </section>
 
-      <Image
-        src="/assets/images/onboarding-img.png"
-        height={1000}
-        width={1000}
-        alt="patient"
-        className="side-img max-w-[50%]"
-      />
+       <Image 
+          src="/assets/images/onboarding-img.png"
+          height={1000}
+          width={1000}
+          alt='patient'
+          className='side-img max-w-[50%]'
+       />
     </div>
-    
-  );
+  )
 }
+
+export default page
